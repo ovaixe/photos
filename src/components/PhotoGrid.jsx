fa90855b2130fe4fc267ea561cd5e558610c0fca
Bhat Owais <owaisbhat996@outlook.com>
2023-12-11 12:02:16 +0530

    Updated Grid and API key

--- src/components/PhotoGrid.jsx
+++ src/components/PhotoGrid.jsx
@@ -32,7 +32,7 @@ export default function PhotoGrid({ handleScroll }) {
       <div
         onClick={handleCloseModel}
         onScroll={handleScroll}
-        className={`p-5 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 scroll-smooth overflow-y-scroll h-grid ${
+        className={`p-5 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-4 scroll-smooth overflow-y-scroll h-grid ${
           modelBoxContext.showModel ? "opacity-50" : ""
         }`}
       >
